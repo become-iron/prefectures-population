fbfb8aee9c642b40f0d0fa89844b02b884b87ab3
Nikita Burkov <nikitaburkov@yandex.ru>
2023-09-25 18:41:25 +0900

    Generate all coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,9 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      coverage: {
-        reporter: ["text", "json-summary", "json"],
-      },
+      // coverage: {
+      //   reporter: ["text", "json-summary", "json"],
+      // },
       environment: "jsdom",
       exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
