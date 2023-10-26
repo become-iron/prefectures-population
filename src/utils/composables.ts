0a45977032009ecb1271d2805b8323703846973d
Nikita Burkov <nikitaburkov@yandex.ru>
2023-10-26 19:48:14 +0900

    Minor edits

--- src/utils/composables.ts
+++ src/utils/composables.ts
@@ -4,7 +4,7 @@ export function useMobile() {
   const mobile = ref()
 
   function update() {
-    mobile.value = window.innerWidth <= 768
+    mobile.value = window.innerWidth <= 768  // approximate "mobile" screen width
   }
 
   update()
